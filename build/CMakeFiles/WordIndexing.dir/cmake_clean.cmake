683c9c54c22a1228549253131b8d53daad3f80ad
Farid Oubbati <f.oubbati@yahoo.f>
2019-10-06 13:54:25 +0200

    initial working project commit

---
+++ build/CMakeFiles/WordIndexing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordIndexing.dir/app/main.cpp.o"
+  "bin/WordIndexing.pdb"
+  "bin/WordIndexing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordIndexing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/WordIndexingLib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordIndexingLib.dir/SearchThread.cpp.o"
+  "CMakeFiles/WordIndexingLib.dir/SyncQueue.cpp.o"
+  "CMakeFiles/WordIndexingLib.dir/WorkerThread.cpp.o"
+  "../lib/libWordIndexingLib.pdb"
+  "../lib/libWordIndexingLib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WordIndexingLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
